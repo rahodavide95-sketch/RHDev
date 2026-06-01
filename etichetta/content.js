4c5c922c76aa8f75bbf6959c16f8756f6b8a2dde
rahodavide95-sketch <rahodavide95@gmail.com>
2026-06-01 12:35:10 +0200

    Update content.js

--- etichetta/content.js
+++ etichetta/content.js
@@ -35,7 +35,7 @@ const SITE = {
       year:    "2026",
       catalog: "SSSCLTR001",
       format:  "Digital",
-      cover:   "etichetta/images/releases/sc001.jpg",
+      cover:   "images/releases/sc001.jpg",
       link:    "https://fanlink.tv/raho-solvane-ep-subconscious-culture",
     },
     {
